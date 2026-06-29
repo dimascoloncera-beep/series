77ef2aef0f0e205f2292dcdbf2a432d7bc1f5adf
dimascoloncera-beep <dimascoloncera@gmail.com>
2026-06-28 21:20:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
-st.title("Grafica de series de tiempo")
 
-serie = [10, 15, 18, 26, 31]
+st.title("Gráfica de serie de tiempo")
+
+entrada = st.text_input(
+    "Ingrese la serie, separada por comas:",
+    value="10,15,18,26,31"
+)
+
+# Divide el texto y elimina espacios
+entrada2 = [x.strip() for x in entrada.split(",")]
+
+# Convierte a números
+serie = [float(i) for i in entrada2]
+
+# Muestra la lista
+st.write("Serie:", serie)
+
+# Grafica
 st.line_chart(serie)
